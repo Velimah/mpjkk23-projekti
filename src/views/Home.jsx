f1c426ea949efe1ed35f214f9d99a8df1db5f7c9
yliysi <catrina.koskinen@gmail.com>
2023-04-20 00:31:18 +0300

    update frontpage
    changed:
    - favicon and title (favicon needs to be different color?)
    - changed themeOptions, added primary and secondary colors, added font settings, and override border radius for buttons globally

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,13 +1,73 @@
-import {Typography} from '@mui/material';
+import {Grid, Button, Typography} from '@mui/material';
 import {Box} from '@mui/system';
 import MediaTable from '../components/MediaTable';
+import {Link} from 'react-router-dom';
 
 const Home = () => {
   return (
     <>
-      <Box sx={{maxWidth: 'lg', margin: 'auto'}}>
-        <Typography component="h1" variant="h2" textAlign="center" sx={{my: 6}}>
-          Home
+      <Grid
+        container
+        columnSpacing={{xs: 1, sm: 2, md: 3}}
+        direction="row"
+        justifyContent="center"
+        alignItems="center"
+        sx={{py: '80px', backgroundColor: '#E3A7B6'}}
+      >
+        <Grid item sx={5}>
+          <Box sx={{maxWidth: '500px'}}>
+            <img
+              src={'/src/assets/onlycats_illustration1.png'}
+              alt={'Cat illustration'}
+              loading="lazy"
+              width="100%"
+            />
+          </Box>
+        </Grid>
+        <Grid item sx={1}>
+          <Box
+            sx={{
+              borderRadius: '25px',
+              backgroundColor: 'white',
+              boxShadow: 3,
+              p: '40px',
+              width: '300px',
+            }}
+          >
+            <Typography
+              component="h1"
+              variant="h1"
+              textAlign="center"
+              sx={{mb: 5}}
+            >
+              Share and discover cat photos on OnlyCats.
+            </Typography>
+            <Typography component="p" textAlign="center" sx={{mb: 5}}>
+              Join our community and connect with fellow cat lovers.
+            </Typography>
+            <Box textAlign="center">
+              <Button
+                variant="contained"
+                size="large"
+                component={Link}
+                to="/login"
+                sx={{mb: 1}}
+              >
+                Register
+              </Button>
+            </Box>
+            <Typography component="p" textAlign="center">
+              Already have an account?{' '}
+              <Button variant="text" component={Link} to="/login">
+                Log in
+              </Button>
+            </Typography>
+          </Box>
+        </Grid>
+      </Grid>
+      <Box sx={{my: '50px', mx: '10%'}}>
+        <Typography component="h2" variant="h2" sx={{mb: 2}}>
+          Discover cats
         </Typography>
         <MediaTable />
       </Box>
